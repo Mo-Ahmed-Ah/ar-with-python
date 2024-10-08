33b757afdd8b28c84ac8cc9038690a530cac90f5
Mo-Ahmed-Ah <ahmed0033ahmed.mo@gmail.com>
2024-10-08 12:05:11 +0300

    add marker detection and add select centur

--- code/CREATE_MARKER/main.py
+++ code/CREATE_MARKER/main.py
@@ -1,22 +1,13 @@
-import cv2 as cv
+import cv2 as cv 
 from cv2 import aruco
-import numpy as np
 
-# تحديد القاموس المستخدم
-marker_dice = aruco.Dictionary_get(aruco.DICT_4X4_50)
+marker_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
 
-MARKER_SIZE = 400
+MARKER_SIZE =200 # pixels
 
 for id in range(20):
-    # إنشاء صورة بيضاء لرسم الماركر عليها
-    marker_image = np.ones((MARKER_SIZE, MARKER_SIZE), dtype=np.uint8) * 255
-    
-    # رسم الماركر على الصورة
-    marker_image = aruco.drawMarker(marker_dice, id, MARKER_SIZE)
-    
-    # عرض الصورة
-    cv.imshow("Marker ID: " + str(id), marker_image)
-    cv.waitKey(0)
-    break
-
-cv.destroyAllWindows()
+    marker_img = aruco.drawMarker(marker_dict , id , MARKER_SIZE)
+    # cv.imshow("test" , marker_img)
+    cv.imwrite(f"markers/markder_{id}.png" , marker_img)
+    # cv.waitKey(0)
+    # break
